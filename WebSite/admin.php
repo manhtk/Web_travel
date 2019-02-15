6ecfa3795bbd23993f008a3573bf109c9c986edd
manhtk <tranducmanhdhbk@gmail.com>
2019-02-15 17:02:54 +0700

    a

--- WebSite/admin.php
+++ WebSite/admin.php
@@ -1,19 +1,10 @@
-      <?php include "view/header.php" ?>
+      <?php include "public/skill/header.php" ?>
                         <!-- Page Heading -->
                         <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+                            <h1 class="h3 mb-0 text-gray-800">Chào mừng Admin</h1>
                            
                         </div>
-                        <table border="1">
-                            <tr>
-                                <td>abc</td>
-                                <td>abc</td>
-                            </tr>
-                        </table>
-                        <!-- Content Row -->
+                      
                         
-                        <!-- Content Row -->
-                        
-                        <!-- Content Row -->
-        <?php include "view/footer.php" ?>                 
+        <?php include "public/skill/footer.php" ?>                 
                  
