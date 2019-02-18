7b82c9e779ac05481180a3ba696c0bfc0a978cd0
Khanh <khanh3gtm@gmail.com>
2019-02-18 10:45:27 +0700

    a

--- WebSite/login/config/db.php
+++
@@ -1,4 +0,0 @@
-<?php
-    session_start();
-    $db=mysqli_connect('localhost','root','','webtravel') or die ('error connect sqlserver');
-?>
--- WebSite/login/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-    header('location:controllers/logincontroller.php')
-?>
