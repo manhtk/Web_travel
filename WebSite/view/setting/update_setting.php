1856d690010f40202e54c4c6040c77a413d6dd03
Khanh <khanh3gtm@gmail.com>
2019-02-19 10:02:20 +0700

    khanh luu file

--- WebSite/view/setting/update_setting.php
+++ WebSite/view/setting/update_setting.php
@@ -7,31 +7,42 @@
 <body>
     <div class="suacaidat">
         <h3>Update Setting</h3>
-        <a href="admin.php?controller=setting">Back</a>
+        
         <form method="post" action="">
             <table border="0">
                 
                     <td><input type="hidden" name="setting[]" value="<?php echo $value['id'] ?>" placeholder="id"></td>
                 <tr>
                     <td>Date Format</td>
-                </tr>
+                
                     <td><input type="text" name="setting[]" value="<?php echo $value['dateformat'] ?>" placeholder="date format"></td>
+                </tr>
                 <tr>
                     <td>Logo</td>
-                </tr>
+                
                     <td><input type="text" name="setting[]" value="<?php echo $value['logo'] ?>" placeholder="logo"></td>
+                </tr>
                 <tr>
                     <td>Email Admin</td>
-                </tr>
+                
                     <td><input type="email" name="setting[]" value="<?php echo $value['admin_email'] ?>" placeholder="email admin"></td>
+                </tr>
                 <tr>
                     <td>Paging</td>
-                </tr>
+                
                     <td><input type="number" name="setting[]" min = "1" value="<?php echo $value['paging'] ?>" placeholder="paging">
+                
                 </td>
-                    <td><input type="submit" name="update_setting" value="Update Data"></td>
+            </tr>
+                    
+
             </table>
         </form>
+            <div style="text-align: center;padding:10px; ">
+            <td><button onclick="location.href='admin.php?controller=setting'" type="button" style="margin-right: 18px;">Cancel</button>
+            <td><input type="submit" name="update_setting" value="Update"></td>
+        </td></div>
+        
     </div>
 </body>
 </html>
