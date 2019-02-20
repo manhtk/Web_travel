fa74769447d394315edfaf2229faeee95fd0a987
Khanh <khanh3gtm@gmail.com>
2019-02-20 09:29:16 +0700

    khanh Sky

--- WebSite/controller/room/index.php
+++ WebSite/controller/room/index.php
@@ -32,9 +32,19 @@ switch ($action) {
                $tbl1 = "hotel";
                $id = "hotel_id";
             
-            $data = $db->getALLDataBase($tbl1,$tbl2,$id);
-             require_once('view/room/list_room.php');
-            break;   
+            $limit = $db->getPag();
+            echo $limit;
+            $paged = isset($_GET['page']) ? $_GET['page'] : 1;
+            if(empty($paged) || !is_numeric((float)$paged))
+                $paged = 1;
+
+            $offset = ($paged - 1) * $limit;
+
+            $data = $db->getAllDataBase($tbl1,$tbl2,$id,$offset,$limit);
+
+            $count = $db->phantrang($tbl2);
+            require_once("view/setting/test-phantrang.php");
+            break;
     default:
         {
             
