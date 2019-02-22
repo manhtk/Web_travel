4e465dd68a8f23852db5ef338d56998297037a45
cuongth97 <thocuong97@gmail.com>
2019-02-22 08:59:58 +0700

    cuong

---
+++ WebSite/controller/user/index.php
@@ -0,0 +1,109 @@
+<?php
+include_once 'model/DB.php';
+if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+} else {
+    $action = " ";
+}
+
+switch ($action) {
+    case "add":
+    {
+       if(isset($_POST['add_user']))
+            {
+                $value = $_POST["user"];
+                $table = "user";
+                if(!$db->insertData($table, $value)) {
+                    echo "Can't insert data because duplicate id of room. Please check again!";
+                    echo "<br>";
+                    echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
+                
+                 } else
+                 {
+                    echo "Successful.";
+                 }
+            }
+            require_once ("view/user/add_user.php");
+            break;
+    }
+        
+            
+    case "update": {  
+           if (isset($_GET['id'])) {
+                $table = 'user';
+                $id = $_GET['id'];
+                $value = $db->getDataUpdate($table, $id);
+                if (isset($_POST['update_user'])) {
+                    $val = $_POST["user"];
+                    if ($db->updateData($table, $id, $val)) {
+                        echo "
+                    <script>
+                        window.location.href ='admin.php?controller=room&action=list';
+                </script>";
+                    } else {
+                        echo "Can't update data because duplicate id of hotel. Please check again!";
+                        echo "<br>";
+                        echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
+                        break;
+                    }
+                }
+            }
+     
+            require_once 'view/room/update_user.php';
+            break;
+
+        }
+    case "search":
+        {
+            if (isset($_GET['key'])) {
+                $key = $_GET['key'];
+                $data_Search = $db->searchData('room', 'hotel', 'hotel_id', 'room_name', $key);
+            }
+            require_once("view/room/search_room.php");
+            break;
+        }    
+    case "delete":
+        {
+
+        }
+     case 'list':
+          
+              
+               $tbl1 = "user";
+               $id = "user_id";
+            
+            $limit = $db->getPag();
+            $paged = isset($_GET['page']) ? $_GET['page'] : 1;
+            if(empty($paged) || !is_numeric((float)$paged))
+                $paged = 1;
+
+            $offset = ($paged - 1) * $limit;
+
+            $data = $db->getAllDataBase($tbl1,$id,$offset,$limit);
+
+            $count = $db->phantrang($tbl1);
+            echo var_dump($offset);
+            require_once("view/user/list_user.php");
+            break;
+    default:
+        {
+            
+             $tbl2 = "room";
+               $tbl1 = "hotel";
+               $id = "hotel_id";
+            
+            $limit = $db->getPag();
+            $paged = isset($_GET['page']) ? $_GET['page'] : 1;
+            if(empty($paged) || !is_numeric((float)$paged))
+                $paged = 1;
+
+            $offset = ($paged - 1) * $limit;
+
+            $data = $db->getAllDataBase($tbl1,$tbl2,$id,$offset,$limit);
+
+            $count = $db->phantrang($tbl2);
+            require_once("view/user/list_user.php");
+            break;
+        }
+}
+?>
---
+++ WebSite/view/user/list_user.php
@@ -0,0 +1,82 @@
+
+<?php include_once "public/skill/header.php" ?>
+<div class="table-responsive">
+	<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+		<div class="row">
+			<div class="col-md-4">
+				<form action="" method="get"
+      class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+    <div  class="input-group" style="border: #dddfeb solid 1px; border-radius: 2px; ">
+        <input type="hidden" name="controller" value="room">
+        <input type="text" name="key" class="form-control bg-light border-0 small"
+               placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" ">
+        <div class="input-group-append">
+            <button class="btn btn-primary" type="submit">
+                <i class="fas fa-search fa-sm"></i>
+            </button>
+        </div>
+
+        <input type="hidden" name="action" value="search">
+    </div>
+</form>
+			</div>
+		</div>
+		<br/>
+		<thead>
+			<tr>
+				<th>STT</th>
+				<th>username</th>
+				<th>password</th>
+				<th>role</th>
+				<th>first_name</th>
+				<th>last_name</th>
+				<th>address</th>
+				<th>email</th>
+				<th>identity_card</th>
+				<th>phone_number</th>
+				<th>birthday</th>
+				<th>gender</th>
+				<th>regisdate</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+			foreach ($data as $value) {
+				?>
+				<tr>
+					<td><?php echo $value['user_id'] ?></td>
+					<td><?php echo $value['username'] ?></td>
+					<td><?php echo $value['password'] ?></td>
+					<td><?php echo $value['role'] ?></td>
+					<td><?php echo $value['first_name'] ?></td>
+					<td><?php echo $value['last_name'] ?></td>
+					<td><?php echo $value['address'] ?></td>
+					<td><?php echo $value['email'] ?></td>
+					<td><?php echo $value['identity_card'] ?></td>
+					
+					<td><?php echo $value['phone_number'] ?></td>
+					<td><?php echo $value['birthday'] ?></td>
+					<td><?php echo $value['gender'] ?></td>
+					<td><?php echo $value['regisdate'] ?></td>
+					
+					<td>
+						<input  type="submit" name="Xóa" value="Xóa" ><input type="submit" name="Sửa" onclick="location.href='admin.php?controller=user&action=update&id=<?php echo $value['user_id']?>'"  value="Sửa">
+					</td>
+				</tr>
+			<?php } ?>
+		</tbody>
+
+	</table>
+</div>
+<?php 
+	$page = $db->phantrang('hotel');
+?>
+
+<div class="test-phantrang">
+	<?php
+	for ($i=1; $i<=$page ; $i++) { 
+		echo "<a href='admin.php?controller=user&action=list&page=$i'>Page $i</a> - ";
+	}
+	?>
+</div>
+<?php include_once "public/skill/footer.php" ?>         
