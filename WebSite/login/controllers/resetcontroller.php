34d08e6cf459e34abd3a6355d62027420763b6de
ThangNMT <minhthang1697@gmail.com>
2019-02-25 17:27:35 +0700

    thang commit 52722519

--- WebSite/login/controllers/resetcontroller.php
+++ WebSite/login/controllers/resetcontroller.php
@@ -1,25 +1,42 @@
 <?php
-
-if (isset($_POST['s']) && $_POST['s']==1) {
-    $errors = array();
-    $password_1= $_POST['password_1'];
-    $password_2= $_POST['password_2'];
-    if (isset($_POST['email'])) {
-        $conn = new mysqli('localhost', 'root', '', 'webtravel');
-        $email = $conn->real_escape_string($_POST['email']);
-        $token = $conn->real_escape_string($_GET['token']);
-        $sql = $conn->query("SELECT user_id FROM user WHERE
+if(isset($_GET['token']) && isset($_GET['email'])) {
+    $token=$_GET['token'];
+    $email=$_GET['email'];
+    $conn = new mysqli('localhost', 'root', '', 'webtravel');
+    $sqlC = $conn->query("SELECT user_id FROM user WHERE
 			email='$email' AND token='$token' AND token<>'' AND tokenExpire > NOW()
 		");
-        if ($sql->num_rows > 0) {
 
-            $newPassword = generateNewString();
-            $newPasswordEncrypted = password_hash($newPassword, PASSWORD_BCRYPT);
-            $conn->query("UPDATE user SET token='', password = '$newPasswordEncrypted'
+
+    if ($sqlC->num_rows > 0) {
+
+        if (isset($_POST['s']) && $_POST['s'] == 1) {
+            $errors = array();
+            $password_1 = $_POST['password_1'];
+            $password_2 = $_POST['password_2'];
+            if (isset($_POST['resetpass'])) {
+                $check = true;
+                if (empty($_POST['password_1'])) {
+                    array_push($errors, '<p class="error">Password is required</p>');
+                    $check = false;
+                }
+                if ($_POST['password_1'] != $_POST['password_2']) {
+                    array_push($errors, '<p class="error">The two passwords do not match</p>');
+                    $check = false;
+                }
+
+                if ($check) {
+                    $password = md5($password_1);
+                    $conn->query("UPDATE user SET token='', password = '$password'
 				WHERE email='$email'
 			");
+                    header('location:../../login');
+                }
+            }
 
         }
+    }else{
+        header('location:../../login');
     }
 }
 ?>
