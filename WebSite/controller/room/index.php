75d4ba4848d12caeafee978c7960fb8d069d35b6
Khanh <khanh3gtm@gmail.com>
2019-03-05 08:56:05 +0700

    a

--- WebSite/controller/room/index.php
+++ WebSite/controller/room/index.php
@@ -205,7 +205,7 @@ switch ($action) {
 
             $data = $db->getAllDataBase($tbl1,$tbl2,$id,$offset,$limit);
 
-            $count = $db->phantrang($tbl2);
+            $count = $db->paging($tbl2);
 
             require_once("view/room/list_room.php");
             break;
