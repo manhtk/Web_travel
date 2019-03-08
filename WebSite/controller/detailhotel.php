901d9ed6706dd8d8282c189091cae10416d66fa0
ThangNMT <minhthang1697@gmail.com>
2019-03-08 09:00:53 +0700

    thang cm

---
+++ WebSite/controller/detailhotel.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rich Boy
+ * Date: 3/6/2019
+ * Time: 11:04 AM
+ */
+class DetailHotel extends Controller{
+    public function view(){
+        $res = $this->model->getHotel(4,'hotel_name');
+        $this->view->render('site/detailhotel',array('data' => $res));
+    }
+
+}
