a0f7a662c35efed95398899f7db19266bab1a477
Dinh Hoa <bunsroi123@gmail.com>
2019-03-20 16:26:04 +0700

    Hoa

--- WebSite/model/homepage.php
+++ WebSite/model/homepage.php
@@ -61,4 +61,55 @@ class HomePage_Model extends Model {
         return $data;
     
   }
+  
+  public function searchData($key, $offset = '', $limit = '')
+    {
+        $orderby = '';
+        if (isset($_GET['optradio']))
+            $orderby = $_GET['optradio'];
+        $sql = "SELECT *,MIN(room.price)AS hotel_price,ROUND(AVG(room.starnum),1) AS 'hotel_point' FROM hotel  
+                                       INNER JOIN city ON hotel.city_id = city.city_id
+                                       INNER JOIN room ON hotel.hotel_id=room.hotel_id 
+                                       WHERE city.city_id='$key'
+                                       GROUP BY hotel.hotel_id";
+
+        
+
+        if (!empty($orderby)) {
+            switch ($orderby) {
+                case 'low':
+                    $sql .= " ORDER BY hotel_price ASC";
+                    break;
+
+                case 'hight':
+                    $sql .= " ORDER BY hotel_price DESC";
+                    break;
+                case 'name_az':
+
+                    $sql .= " ORDER BY hotel.hotel_name ASC";
+                    break;
+                case 'name_za':
+                    $sql .= " ORDER BY hotel.hotel_name DESC";
+                    break;
+
+            }
+        }
+        if ($limit != '') {
+            $sql .= " LIMIT {$offset},{$limit}";
+        }
+
+        $res = $this->query($sql);
+
+        $data = [];
+
+        if ($res && $res->num_rows > 0) {
+            while ($row = $res->fetch_assoc()) {
+                $data[] = $row;
+
+            }
+        }
+
+        return $data;
+    }
+
 }
