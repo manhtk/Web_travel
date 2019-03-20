5e4a0cbf717859d3d839ec07b99ec41e3eb4ffd8
manhtk <tranducmanhdhbk@gmail.com>
2019-03-20 09:53:40 +0700

    manh

--- WebSite/view/site/slidebar_search.php
+++ WebSite/view/site/slidebar_search.php
@@ -191,6 +191,7 @@
                                         $('input[name="date"]').daterangepicker(
                                         {
                                             "autoApply": true,
+                                            "minDate" : moment().startOf('hour'),
                                             "locale": {
                                                 "format": "DD/MM/YYYY",
                                             },
