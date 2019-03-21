b28394a224e4e39ec8a820a946e619f183187bcd
Khanh <khanh3gtm@gmail.com>
2019-03-21 17:12:47 +0700

    kha

--- WebSite/libs/Controller.php
+++ WebSite/libs/Controller.php
@@ -6,21 +6,39 @@
  	{
  		//Khởi tạo đối tượng trong controll base
  		session_start();
- 		$this->view = new View();
+ 		$this->view = new View($this->getCartData());
+
  	}
  	public function getSiteURL(){
  		$site_url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/';
  		return $site_url;
  	}
  	//Phương thức load Model trong controller
- 	public function loadModel($name)
+ 	public function loadModel($name, $return = false)
  	{
  		$file_model = 'model/'. $name . '.php';
  		if(file_exists($file_model)){
  			require_once $file_model;
  			$name_model = $name .'_model';
- 			$this->model = new $name_model();
+ 			if(!$return)
+ 				$this->model = new $name_model();
+ 			else{
+ 				$c_model = new $name_model();
+ 				return $c_model;
+ 			}
  		}
+ 	}
+
+ 	public function getCartData(){
+ 		if(isset($_SESSION['st_cart'])){
+ 			$ss = $_SESSION['st_cart'];
+ 			$cModel = $this->loadModel('bookcart', true);
+ 			$dt_cart = $cModel->getRoomDetal($ss['room_id']);
+ 			//dd($dt_cart);
+ 			return $dt_cart;
+ 		}
+ 		return '';
+ 		
  	}
  	
  }
--- WebSite/libs/View.php
+++ WebSite/libs/View.php
@@ -1,9 +1,14 @@
 <?php 
   
 class View{
+	public $cart = '';
+	public function __construct($_cart = array()){
+		$this->cart = $_cart;
+	}
 	public function render($name,$data,$noInclude = false)
 	{
 		extract($data);
+		$cart_data = $this->cart;
 		if($noInclude == true){
 			include 'view/' . $name . '.php';
 		}
